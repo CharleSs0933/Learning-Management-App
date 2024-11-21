f43b74d620d20654eec92a63489dd60c723abe79
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-11-21 23:24:26 +0700

    Landing Page

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -1,4 +1,4 @@
-import { BookOpen } from "lucide-react";
+import { Bell, BookOpen } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -6,18 +6,34 @@ const NonDashboardNavbar = () => {
   return (
     <nav className="nondashboard-navbar">
       <div className="nondashboard-navbar__container">
-        <Link href="/" className="nondashboard-navbar__brand">
-          CHARLESS
-        </Link>
-        <div className="flex items-center gap-4">
-          <div className="relative group">
-            <Link href="/search" className="nondashboard-navbar__search-input">
-              <span className="hidden sm:inline">Search Courses</span>
-              <span className="sm:hidden">Search</span>
-            </Link>
-            <BookOpen className="nondashboard-navbar__search-icon" size={10} />
+        <div className="nondashboard-navbar__search">
+          <Link href="/" className="nondashboard-navbar__brand">
+            CHARLESS
+          </Link>
+          <div className="flex items-center gap-4">
+            <div className="relative group">
+              <Link
+                href="/search"
+                className="nondashboard-navbar__search-input"
+              >
+                <span className="hidden sm:inline">Search Courses</span>
+                <span className="sm:hidden">Search</span>
+              </Link>
+              <BookOpen
+                className="nondashboard-navbar__search-icon"
+                size={18}
+              />
+            </div>
           </div>
         </div>
+      </div>
+      <div className="nondashboard-navbar__actions">
+        <button className="nondashboard-navbar__notification-button">
+          <span className="nondashboard-navbar__notification-indicator"></span>
+          <Bell className="nondashboard-navbar__notification-icon" />
+        </button>
+
+        {/* SIGN IN BUTTONS */}
       </div>
     </nav>
   );
