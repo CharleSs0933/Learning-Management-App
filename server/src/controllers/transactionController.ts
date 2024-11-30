64e8ed68ca311dab8e235539d476f6845f1869ec
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-11-30 17:10:44 +0700

    get list transactions

--- server/src/controllers/transactionController.ts
+++ server/src/controllers/transactionController.ts
@@ -107,3 +107,25 @@ export const createTransaction = async (
       .json({ message: "Error creating transaction and enrollment", error });
   }
 };
+
+export const listTransactions = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req.query;
+
+  try {
+    const transactions = userId
+      ? await Transaction.query("userId").eq(userId).exec()
+      : await Transaction.scan().exec();
+
+    res.json({
+      message: "Transaction retrieved successfully",
+      data: {
+        transactions,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving transactions", error });
+  }
+};
